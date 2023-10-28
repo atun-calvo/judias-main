99fff4a16633fa27a7912999a8b919a21e46a372
atun-calvo <148367224+atun-calvo@users.noreply.github.com>
2023-10-28 10:51:41 +0200

    Update eventos.sh

--- eventos.sh
+++ eventos.sh
@@ -26,10 +26,9 @@ if [[ $(echo $?) == 0 ]]
 fi
 
 # Ejecutamos tor en segundo plano para que se inicie
-systemctl enable --now tor.service &> /dev/null
 #pkill tor &> /dev/null
-#tor & > /dev/null 2>&1
-#sleep 10
+tor & > /dev/null 2>&1
+sleep 15
 
 ##Variables##
 rm -f /tmp/tmp.* 2> /dev/null
